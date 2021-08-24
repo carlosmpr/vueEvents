c425806518092814ed3305a025bee9497524d849
Carlos Polanco <carlospolanco@Carloss-Mac-mini.local>
2021-08-24 17:45:10 -0400

    vue Events

---
+++ app.js
@@ -0,0 +1,30 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      counter: 0,
+      name: ''
+    };
+  },
+  computed:{
+    fullname(){
+      
+    }
+  },
+  methods: {
+    setName(event) {
+      this.name = event.target.value;
+    },
+    add(num) {
+      this.counter = this.counter + num;
+    },
+    reduce(num) {
+      this.counter = this.counter - num;
+      // this.counter--;
+    },
+    resetInput(){
+      this.name = ''
+    }
+  }
+});
+
+app.mount('#events');
